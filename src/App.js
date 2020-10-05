20945582b323fc2626598b9647f0fe1d4a5a6429
balaji.jagannathan <balaji.jagannathan@teledirectasia.com>
2020-10-05 13:33:47 +0800

    Adding sample routing pages as explained in docs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Fragment } from 'react';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+const browserHistory = createBrowserHistory();
+
+import {
+  Dashboard,
+  SignIn
+} from './modules';
+
 export default function App() {
   return (
     <Fragment>
-      <h1>Welcome</h1>
-	  <h2>Hello World!</h2>
+      <Router history={browserHistory}>
+        <h1>Simple Banking App goes here</h1>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/">
+            <SignIn />
+          </Route>
+        </Switch>
+      </Router>
     </Fragment>
   );
 };
