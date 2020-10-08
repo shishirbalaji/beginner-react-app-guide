5e09be6a518ffe70ca9249bb909d281ba801b6cc
balaji.jagannathan <balaji.jagannathan@teledirectasia.com>
2020-10-08 14:17:32 +0800

    Adding Material UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Fragment } from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './theme';
+
+// Component.
+import Header from './components/Header';
 
 const browserHistory = createBrowserHistory();
 
@@ -13,17 +18,19 @@ import {
 export default function App() {
   return (
     <Fragment>
-      <Router history={browserHistory} basename="/beginner-react-app-guide">
-        <h1>Simple Banking App goes here</h1>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/">
-            <SignIn />
-          </Route>
-        </Switch>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Router history={browserHistory}>
+          <Switch>
+            <Route exact path="/">
+              <SignIn />
+            </Route>
+            <Route exact path="/dashboard">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
     </Fragment>
   );
 };
